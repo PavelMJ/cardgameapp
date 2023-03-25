fac1d3e42abcbff35b0d213dbeea9e18e93908a6
PavelMj <pavelmat@hotmail.com>
2023-03-25 09:13:51 +0300

    value

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,33 +3,33 @@ import React from 'react'
 export default function Table(props) {
 	return (
 		<div className='table'>
-		<ul className='players'>
-			<li>Player</li>
-		{props.playersData.map((obj, index)=>(
-			<li key={index}>{obj.playerName}</li>
-		))}
-	</ul>
-	<ul className='players'>
-			<li>Games</li>
-		{props.playersData.map((obj, index)=>(
-			<li className='data' key={index}>{obj.games}</li>
-		))}
-	</ul>
+			<ul className='players'>
+				<li>Player</li>
+				{props.playersData.map((obj, index) => (
+					<li key={index}>{obj.playerName}</li>
+				))}
+			</ul>
+			<ul className='players'>
+				<li>Games</li>
+				{props.playersData.map((obj, index) => (
+					<li className='data' key={index}>{obj.games}</li>
+				))}
+			</ul>
 
-	<ul className='players'>
-			<li>Wins</li>
-		{props.playersData.map((obj, index)=>(
-			<li className='data' key={index}>{obj.win}</li>
-		))}
-	</ul>
+			<ul className='players'>
+				<li>Wins</li>
+				{props.playersData.map((obj, index) => (
+					<li className='data' key={index}>{obj.win}</li>
+				))}
+			</ul>
 
-	<ul className='players'>
-			<li>Loses</li>
-		{props.playersData.map((obj, index)=>(
-			<li className='data' key={index}>{obj.lose}</li>
-		))}
-	</ul>
+			<ul className='players'>
+				<li>Loses</li>
+				{props.playersData.map((obj, index) => (
+					<li className='data' key={index}>{obj.lose}</li>
+				))}
+			</ul>
 
-	</div>
+		</div>
 	)
 }
